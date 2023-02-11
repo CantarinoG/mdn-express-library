77613d7f2c591cc7b1816cb79b9c59a7b57163c3
CantarinoG <guilhermecantarino1709@gmail.com>
2023-02-11 19:21:36 -0300

    Add models

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Set up mongoose connection
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const mongoDB = "mongodb+srv://admin:admin123@cluster0.h5ykkoj.mongodb.net/local_library?retryWrites=true&w=majority";
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -39,3 +50,5 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+//mongodb+srv://admin:admin123@cluster0.h5ykkoj.mongodb.net/local_library?retryWrites=true&w=majority
