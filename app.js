d5de122cdd356989e996243243f2c1f816a1f4fc
CantarinoG <guilhermecantarino1709@gmail.com>
2023-02-15 12:37:02 -0300

    Change env variables

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ var app = express();
 //Set up mongoose connection
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
-const mongoDB = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.h5ykkoj.mongodb.net/local_library?retryWrites=true&w=majority`;
+
+const dev_db_url =
+  "mongodb+srv://your_user_name:your_password@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 main().catch(err => console.log(err));
 
